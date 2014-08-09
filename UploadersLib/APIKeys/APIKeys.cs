6036e957587e3be36c779f3d224a48ef37f3823f
Jaex <Jaex@users.noreply.github.com>
2014-08-09 03:01:10 +0300

    Adding OneDrive api keys

--- UploadersLib/APIKeys/APIKeys.cs
+++ UploadersLib/APIKeys/APIKeys.cs
@@ -54,6 +54,8 @@ namespace UploadersLib
         public static string JiraConsumerKey = "";
         public static string MediaFireAppId = "";
         public static string MediaFireApiKey = "";
+        public static string OneDriveClientID = "";
+        public static string OneDriveClientSecret = "";
 
         // Text Uploaders
         public static string PastebinKey = "";
