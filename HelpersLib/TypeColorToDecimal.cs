fe44f20fc9194348f5847d592a910d0026d7b140
ConfusedGiant <dorias.ahmed@gmail.com>
2014-11-25 06:44:41 -0500

    Implemented Strategy Pattern on various color formats in HelpersLib.

---
+++ HelpersLib/TypeColorToDecimal.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace HelpersLib
+{
+    class TypeColorToDecimal : iColorFormatType
+    {
+        public int ColorFormat(Color color, String hex, int dec)
+        {
+            ColorFormat format = HelpersLib.ColorFormat.RGB;
+
+            switch (format)
+            {
+                default:
+                case HelpersLib.ColorFormat.RGB:
+                    return color.R << 16 | color.G << 8 | color.B;
+                case HelpersLib.ColorFormat.RGBA:
+                    return color.R << 24 | color.G << 16 | color.B << 8 | color.A;
+                case HelpersLib.ColorFormat.ARGB:
+                    return color.A << 24 | color.R << 16 | color.G << 8 | color.B;
+            }
+        }
+
+        public TypeColorToDecimal()
+        {
+        }
+    }
+}
---
+++ HelpersLib/TypeDecimalToColor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace HelpersLib
+{
+    class TypeDecimalToColor : iColorFormatType
+    {
+        public Color ColorFormat(Color color, String hex, int dec)
+        {
+            ColorFormat format = HelpersLib.ColorFormat.RGB;
+
+            switch (format)
+            {
+                default:
+                case HelpersLib.ColorFormat.RGB:
+                    return Color.FromArgb((dec >> 16) & 0xFF, (dec >> 8) & 0xFF, dec & 0xFF);
+                case HelpersLib.ColorFormat.RGBA:
+                    return Color.FromArgb(dec & 0xFF, (dec >> 24) & 0xFF, (dec >> 16) & 0xFF, (dec >> 8) & 0xFF);
+                case HelpersLib.ColorFormat.ARGB:
+                    return Color.FromArgb((dec >> 24) & 0xFF, (dec >> 16) & 0xFF, (dec >> 8) & 0xFF, dec & 0xFF);
+            }
+        }
+
+        public TypeDecimalToColor()
+        {
+        }
+    }
+}
