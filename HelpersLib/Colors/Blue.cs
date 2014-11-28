fbb02740e3eb9119a1e8c2163bf9a95812c58abc
tanjil <tanjil_matin@hotmail.com>
2014-11-28 14:26:08 -0500

    Implemented Strategy Pattern within HelpersLib
    Pulled the SetBoxMarker, SetSliderMarker, GetBoxColor, GetSliderColor
    from ColorUserControl.cs  Placed them in iDrawStyles interface and had
    the six draw styles classes made (Hue, Saturation, Brightness, Red,
    Green, Blue) to implement them. An instance is made in ColorUserControl,
    and is used to remove the switch statements from the functions that were
    pulled to he interface.

---
+++ HelpersLib/Colors/Blue.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+
+namespace HelpersLib
+{
+    class Blue : iDrawStyles
+    {
+        public override void SetBoxMarker(Point lastPos, MyColor SelectedColor, int ClientWidth, int ClientHeight)
+        {
+            lastPos.X = Round((ClientWidth - 1) * (double)SelectedColor.RGBA.Red / 255);
+            lastPos.Y = Round((ClientHeight - 1) * (1.0 - (double)SelectedColor.RGBA.Green / 255));
+        }
+        public override void SetSliderMarker(Point lastPos, MyColor SelectedColor, int ClientHeight)
+        {
+            lastPos.Y = (ClientHeight - 1) - Round((ClientHeight - 1) * (double)SelectedColor.RGBA.Blue / 255);
+        }
+        public override void GetBoxColor(Point lastPos, MyColor selectedColor, int ClientWidth, int ClientHeight)
+        {
+            selectedColor.RGBA.Red = Round(255 * (double)lastPos.X / (ClientWidth - 1));
+            selectedColor.RGBA.Green = Round(255 * (1.0 - (double)lastPos.Y / (ClientHeight - 1)));
+            selectedColor.RGBAUpdate();
+        }
+        public override void GetSliderColor(Point lastPos, MyColor selectedColor, int ClientWidth, int ClientHeight)
+        {
+            selectedColor.RGBA.Blue = 255 - Round(255 * (double)lastPos.Y / (ClientHeight - 1));
+            selectedColor.RGBAUpdate();
+        }
+    }
+}
---
+++ HelpersLib/Colors/Brightness.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+
+namespace HelpersLib
+{
+    class Brightness : iDrawStyles
+    {
+        public override void SetBoxMarker(Point lastPos, MyColor SelectedColor, int ClientWidth, int ClientHeight)
+        {
+            lastPos.X = Round((ClientWidth - 1) * SelectedColor.HSB.Hue);
+            lastPos.Y = Round((ClientHeight - 1) * (1.0 - SelectedColor.HSB.Saturation));
+        }
+        public override void SetSliderMarker(Point lastPos, MyColor SelectedColor, int ClientHeight)
+        {
+            lastPos.Y = (ClientHeight - 1) - Round((ClientHeight - 1) * SelectedColor.HSB.Brightness);
+        }
+        public override void GetBoxColor(Point lastPos, MyColor selectedColor, int ClientWidth, int ClientHeight)
+        {
+            selectedColor.HSB.Hue = (double)lastPos.X / (ClientWidth - 1);
+            selectedColor.HSB.Saturation = 1.0 - (double)lastPos.Y / (ClientHeight - 1);
+            selectedColor.HSBUpdate();
+        }
+        public override void GetSliderColor(Point lastPos, MyColor selectedColor, int ClientWidth, int ClientHeight)
+        {
+            selectedColor.HSB.Brightness = 1.0 - (double)lastPos.Y / (ClientHeight - 1);
+            selectedColor.HSBUpdate();
+        }
+    }
+}
