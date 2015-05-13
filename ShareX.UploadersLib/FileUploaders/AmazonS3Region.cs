2f2c8f53ecaff97ecfe348c9b4f504c9e091d7a8
Alan Edwardes <alan@alanedwardes.com>
2015-05-13 23:46:39 +0100

    Made Amazon S3 uploader more robust, additional error handling. Moved classes to their own files.

---
+++ ShareX.UploadersLib/FileUploaders/AmazonS3Region.cs
@@ -0,0 +1,32 @@
+﻿using Amazon;
+
+namespace ShareX.UploadersLib.FileUploaders
+{
+    public class AmazonS3Region
+    {
+        public AmazonS3Region(string name)
+        {
+            Name = name;
+        }
+
+        public AmazonS3Region(string name, string identifier, string hostname)
+        {
+            Name = name;
+            Identifier = identifier;
+            Hostname = hostname;
+        }
+
+        public AmazonS3Region(RegionEndpoint region)
+        {
+            Name = region.DisplayName;
+            Identifier = region.SystemName;
+            AmazonRegion = region;
+            Hostname = region.GetEndpointForService("s3").Hostname;
+        }
+
+        public string Name { get; private set; }
+        public string Identifier { get; private set; }
+        public RegionEndpoint AmazonRegion { get; private set; }
+        public string Hostname { get; private set; }
+    }
+}
---
+++ ShareX.UploadersLib/FileUploaders/AmazonS3Settings.cs
@@ -0,0 +1,14 @@
+﻿namespace ShareX.UploadersLib.FileUploaders
+{
+    public class AmazonS3Settings
+    {
+        public string AccessKeyID { get; set; }
+        public string SecretAccessKey { get; set; }
+        public string Endpoint { get; set; }
+        public string Bucket { get; set; }
+        public string ObjectPrefix { get; set; }
+        public bool UseCustomCNAME { get; set; }
+        public string CustomDomain { get; set; }
+        public bool UseReducedRedundancyStorage { get; set; }
+    }
+}
