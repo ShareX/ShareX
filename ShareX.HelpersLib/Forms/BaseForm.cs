4ca6665aeef5b00d43c0df7dc6370d6fa26c111d
Jaex <Jaex@users.noreply.github.com>
2015-09-01 13:43:03 +0300

    Removed BaseForm designer

--- ShareX.HelpersLib/Forms/BaseForm.cs
+++ ShareX.HelpersLib/Forms/BaseForm.cs
@@ -1,13 +1,50 @@
-﻿using System.Windows.Forms;
+﻿#region License Information (GPL v3)
+
+/*
+    ShareX - A program that allows you to take screenshots and share any file type
+    Copyright (c) 2007-2015 ShareX Team
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+    Optionally you can also view the license at <http://www.gnu.org/licenses/>.
+*/
+
+#endregion License Information (GPL v3)
+
+using System.Drawing;
+using System.Windows.Forms;
 
 namespace ShareX.HelpersLib
 {
-    public partial class BaseForm : Form
+    public class BaseForm : Form
     {
-        public BaseForm()
+        protected BaseForm()
         {
             InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            AutoScaleDimensions = new SizeF(6F, 13F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(300, 300);
             Icon = ShareXResources.Icon;
+            Name = "BaseForm";
+            Text = "ShareX";
+            ResumeLayout(false);
         }
     }
 }
