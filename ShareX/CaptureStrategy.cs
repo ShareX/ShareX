502dc86701c5b77feece1faed4cce871cd084886
Roberto Ruffolo <robertoruffolo@hotmail.com>
2015-11-29 18:17:00 -0500

    Added template for all CaptureStrategies

---
+++ ShareX/CaptureStrategy.cs
@@ -0,0 +1,14 @@
+﻿using ShareX.HelpersLib;
+using ShareX.ScreenCaptureLib;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace ShareX
+{
+    abstract class CaptureStrategy
+    {
+        abstract public void capture(CaptureType captureType, TaskSettings taskSettings, bool autoHideForm = true);
+    }
+}
+
