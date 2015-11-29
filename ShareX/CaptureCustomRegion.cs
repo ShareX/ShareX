75dd1710d8c3dabfffcd955cf8cce36e81997566
Roberto Ruffolo <robertoruffolo@hotmail.com>
2015-11-29 18:35:04 -0500

    Added the CaptureStrategy subclass for CaptureCustomRegion

---
+++ ShareX/CaptureCustomRegion.cs
@@ -0,0 +1,30 @@
+﻿using ShareX.HelpersLib;
+using ShareX.ScreenCaptureLib;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ShareX
+{
+    class CaptureCustomRegion : CaptureStrategy
+    {
+        public CaptureCustomRegion(MainForm mainForm)
+            : base(mainForm)
+        { }
+
+        public override void capture(CaptureType captureType, TaskSettings taskSettings, bool autoHideForm = true)
+        {
+            DoCapture(() =>
+            {
+                Rectangle regionBounds = taskSettings.CaptureSettings.CaptureCustomRegion;
+                Image img = Screenshot.CaptureRectangle(regionBounds);
+
+                return img;
+            }, CaptureType.CustomRegion, taskSettings, autoHideForm);
+        }
+
+    }
+}
