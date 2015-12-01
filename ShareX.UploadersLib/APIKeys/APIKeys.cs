1072d267440c55024b74d9c6517b91d4864c1edb
Harley <harley.1011@gmail.com>
2015-12-01 18:03:06 -0500

    Removed api keys

--- ShareX.UploadersLib/APIKeys/APIKeys.cs
+++ ShareX.UploadersLib/APIKeys/APIKeys.cs
@@ -3,21 +3,17 @@
 /*
     ShareX - A program that allows you to take screenshots and share any file type
     Copyright (c) 2007-2015 ShareX Team
-
     This program is free software; you can redistribute it and/or
     modify it under the terms of the GNU General Public License
     as published by the Free Software Foundation; either version 2
     of the License, or (at your option) any later version.
-
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
-
     You should have received a copy of the GNU General Public License
     along with this program; if not, write to the Free Software
     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-
     Optionally you can also view the license at <http://www.gnu.org/licenses/>.
 */
 
@@ -31,8 +27,8 @@ namespace ShareX.UploadersLib
         public static string ImageShackKey = "";
         public static string TinyPicID = "";
         public static string TinyPicKey = "";
-        public static string ImgurClientID = "2a00c2a4245a5ff";
-        public static string ImgurClientSecret = "9cd6de940d5bfce2e69f1505740bac61fe7fb1a0";
+        public static string ImgurClientID = "";
+        public static string ImgurClientSecret = "";
         public static string FlickrKey = "";
         public static string FlickrSecret = "";
         public static string PhotobucketConsumerKey = "";
@@ -42,8 +38,8 @@ namespace ShareX.UploadersLib
         public static string SomeImageKey = "";
 
         // File Uploaders
-        public static string DropboxConsumerKey = "x6ia6cva0cmrj4t";
-        public static string DropboxConsumerSecret = "plfyzlpyjdd5z1q";
+        public static string DropboxConsumerKey = "";
+        public static string DropboxConsumerSecret = "";
         public static string CopyConsumerKey = "";
         public static string CopyConsumerSecret = "";
         public static string HubicClientID = "";
@@ -71,9 +67,9 @@ namespace ShareX.UploadersLib
         public static string BitlyClientSecret = "";
 
         // Other Services
-        public static string GoogleAPIKey = "AIzaSyCt94bmh9Tv_Hw9n6SmN1KAAuVlFH_xoHk";
-        public static string GoogleClientID = "278467917739-ddreaekcnp12cnm4nd21q4avduhok003.apps.googleusercontent.com";
-        public static string GoogleClientSecret = "bjXLccgJE7E0avk1DtHxVqdq";
+        public static string GoogleAPIKey = "";
+        public static string GoogleClientID = "";
+        public static string GoogleClientSecret = "";
         public static string TwitterConsumerKey = "";
         public static string TwitterConsumerSecret = "";
     }
