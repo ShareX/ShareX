8c01c3cffdb57913868005d1cdb5112db4bdd93c
neko <cronhound@gmail.com>
2015-12-29 01:31:17 +1030

    changed pomf.io to pomf.is
    I have moved pomf.io to pomf.is.

--- ShareX.UploadersLib/FileUploaders/Pomf.cs
+++ ShareX.UploadersLib/FileUploaders/Pomf.cs
@@ -57,7 +57,7 @@ namespace ShareX.UploadersLib.FileUploaders
             new PomfUploader("http://openhost.xyz/upload.php"),
             new PomfUploader("https://pomf.cat/upload.php", "http://a.pomf.cat"),
             new PomfUploader("http://pomf.hummingbird.moe/upload.php", "http://a.pomf.hummingbird.moe"),
-            new PomfUploader("https://pomf.io/upload.php"),
+            new PomfUploader("https://pomf.is/upload.php"),
             new PomfUploader("http://pomf.pl/upload.php"),
             //new PomfUploader("https://pomf.se/upload.php"),
             new PomfUploader("https://sugoi.vidyagam.es/upload.php"),
