f69860d1b7c2800bf5d86a0e8f04df45efa13b54
Jaex <Jaex@users.noreply.github.com>
2016-02-18 13:44:43 +0200

    Added snapie.net

--- ShareX.UploadersLib/ImageUploaders/Chevereto.cs
+++ ShareX.UploadersLib/ImageUploaders/Chevereto.cs
@@ -37,7 +37,8 @@ namespace ShareX.UploadersLib.ImageUploaders
             new CheveretoUploader("http://ultraimg.com/api/1/upload", "3374fa58c672fcaad8dab979f7687397"),
             new CheveretoUploader("http://yukle.at/api/1/upload", "ee24aee90bcd24e39cead57c65044bde"),
             new CheveretoUploader("http://img.patifile.com/api/1/upload", "8320784a9b044510e8c723fb778fe3b7"),
-            new CheveretoUploader("http://boltimg.com/api/1/upload", "8dfbcb7ab9b5258a90be7cf09e361894")
+            new CheveretoUploader("http://boltimg.com/api/1/upload", "8dfbcb7ab9b5258a90be7cf09e361894"),
+            new CheveretoUploader("http://snapie.net/myapi/1/upload", "aff7bd5bf65b7e30b675a430049894b3")
         };
 
         public CheveretoUploader Uploader { get; private set; }
