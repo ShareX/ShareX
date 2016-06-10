f72885aa4b0a7b54c26b3f44be60c1aa38dce791
Jaex <Jaex@users.noreply.github.com>
2016-06-10 04:07:07 +0300

    NuGet update

--- ShareX/Program.cs
+++ ShareX/Program.cs
@@ -53,7 +53,7 @@ namespace ShareX
             }
         }
 
-        public static bool IsBeta { get; } = true;
+        public static bool IsBeta { get; } = false;
 
         public static string Title
         {
