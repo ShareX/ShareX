81ae341c21dca449ff66e31b26d47fea05f29510
Jaex <Jaex@users.noreply.github.com>
2016-06-17 17:52:21 +0300

    fixed #1644: Update Chevereto URL

--- ShareX.UploadersLib/ImageUploaders/Chevereto.cs
+++ ShareX.UploadersLib/ImageUploaders/Chevereto.cs
@@ -62,7 +62,7 @@ namespace ShareX.UploadersLib.ImageUploaders
         public static List<CheveretoUploader> Uploaders = new List<CheveretoUploader>()
         {
             new CheveretoUploader("http://ultraimg.com/api/1/upload", "3374fa58c672fcaad8dab979f7687397"),
-            new CheveretoUploader("http://yukle.at/api/1/upload", "ee24aee90bcd24e39cead57c65044bde"),
+            new CheveretoUploader("http://yukle.at/3/api/1/upload", "bf3f97649acf179ac17e2b3004fa44bc"),
             new CheveretoUploader("http://img.patifile.com/api/1/upload", "8320784a9b044510e8c723fb778fe3b7"),
             new CheveretoUploader("http://boltimg.com/api/1/upload", "8dfbcb7ab9b5258a90be7cf09e361894"),
             new CheveretoUploader("http://snapie.net/myapi/1/upload", "aff7bd5bf65b7e30b675a430049894b3"),
