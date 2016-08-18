6d637402283bc77b15e3c68f1794dcf187f02d74
Jaex <Jaex@users.noreply.github.com>
2016-08-19 02:04:42 +0300

    Release

--- ShareX/Properties/AssemblyInfo.cs
+++ ShareX/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("82E6AC09-0FEF-4390-AD9F-0DD3F5561EFC")]
-[assembly: AssemblyVersion("11.1.0")]
-[assembly: AssemblyFileVersion("11.1.0")]
+[assembly: AssemblyVersion("11.2.0")]
+[assembly: AssemblyFileVersion("11.2.0")]
