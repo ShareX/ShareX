eda36299162f04624411146b6ae443599c33aaff
Jaex <Jaex@users.noreply.github.com>
2016-09-22 02:37:08 +0300

    Fix content path

--- AppVeyor/install.ps1
+++ AppVeyor/install.ps1
@@ -1,4 +1,4 @@
-$content = Get-Content "D:\Dev\GitHub\ShareX\ShareX\ShareX\Properties\AssemblyInfo.cs"
+$content = Get-Content "ShareX\Properties\AssemblyInfo.cs"
 $match = [regex]::Match($content, 'AssemblyVersion\(\"(.+?)\"\)')
 if ($match.Success)
 {
