f1ede27ad43b94b76c47f9125f1e7b00d8b44ab4
Jaex <Jaex@users.noreply.github.com>
2016-09-22 04:17:00 +0300

    Update version on install

--- AppVeyor/before_build.ps1
+++ AppVeyor/before_build.ps1
@@ -1,5 +1,3 @@
-Update-AppveyorBuild -Version "$env:AppVersion.$env:APPVEYOR_BUILD_NUMBER"
-
 if ($env:APPVEYOR_PULL_REQUEST_NUMBER -eq $null)
 {
     Invoke-WebRequest "$env:APIKeys" -OutFile "ShareX.UploadersLib\APIKeys\APIKeysLocal.cs"
--- AppVeyor/install.ps1
+++ AppVeyor/install.ps1
@@ -3,6 +3,7 @@ $match = [regex]::Match($content, 'AssemblyVersion\(\"(.+?)\"\)')
 if ($match.Success)
 {
     $env:AppVersion = $match.Groups[1].Value
+    Update-AppveyorBuild -Version "$env:AppVersion.$env:APPVEYOR_BUILD_NUMBER"
 }
 else
 {
