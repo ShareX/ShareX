752cf120ede261d8200cdbd1c56a4b0f2f82ded2
Jaex <Jaex@users.noreply.github.com>
2016-11-11 12:53:47 +0300

    Only compile Steam build on tag push

--- AppVeyor/after_build.ps1
+++ AppVeyor/after_build.ps1
@@ -4,7 +4,7 @@ if ($env:APPVEYOR_PULL_REQUEST_NUMBER -eq $null)
     {
         & "ShareX.Setup\bin\Release\ShareX.Setup.exe" -AppVeyorRelease
     }
-    elseif ($env:CONFIGURATION -eq "Steam")
+    elseif ($env:CONFIGURATION -eq "Steam" -and $env:APPVEYOR_REPO_TAG -eq $true)
     {
         & "ShareX.Setup\bin\Steam\ShareX.Setup.exe" -AppVeyorSteam
     }
