c4929170fac66a9d3c6d54aa04bff32434d5c728
lenovo-pc <lenovo-pc>
2017-08-16 14:07:29 +0800

    add unit testing

---
+++ ShareX.UnitTest/Helpers/ImageHelpersTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ShareX.HelpersLib;
+namespace ShareX.UnitTest
+{
+    [TestClass]
+    public class ImageHelpersTest
+    {
+        [TestMethod]
+        public void ResizeImageTest()
+        {
+            int expectedWidth = 100;
+            int expectedHeight = 100;
+            
+            Image originalImage = new Bitmap("C:\\Users\\horace92\\Pictures\\city.jpg");
+            Image newImage = ImageHelpers.ResizeImage(originalImage, 100, 100);
+
+            Assert.AreEqual(expectedWidth, newImage.Width);
+            Assert.AreEqual(expectedHeight, newImage.Height);
+        }
+
+        [TestMethod]
+        public void LoadImageTest()
+        {
+            Image newImage = ImageHelpers.LoadImage("C:\\Users\\horace92\\Pictures\\city.jpg");
+            Assert.IsNotNull(newImage);
+        }
+    }
+}
