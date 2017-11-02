e289a6ee26962933307b7a762ea85eb3ca0bd71f
Jaex <Jaex@users.noreply.github.com>
2017-11-02 14:35:17 +0300

    Output AppVeyor artifacts to parent folder that way file name won't have directory path

--- ShareX.Setup/Program.cs
+++ ShareX.Setup/Program.cs
@@ -71,7 +71,7 @@ namespace ShareX.Setup
         private static string WindowsStoreDir => Path.Combine(BinDir, "WindowsStore");
         private static string WindowsStoreDebugDir => Path.Combine(BinDir, "WindowsStoreDebug");
 
-        private static string OutputDir => Path.Combine(ParentDir, "Output");
+        private static string OutputDir => AppVeyor ? ParentDir : Path.Combine(ParentDir, "Output");
         private static string PortableOutputDir => Path.Combine(OutputDir, "ShareX-portable");
         private static string SteamOutputDir => Path.Combine(OutputDir, "ShareX-Steam");
         private static string WindowsStoreOutputDir => Path.Combine(OutputDir, "ShareX-WindowsStore");
