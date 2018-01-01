8d9f486f3902e4d6ed01bd951b44724b2c9fc976
Charles Milette <charles.milette@gmail.com>
2018-01-01 12:32:18 -0500

    Use a SharedAssemblyInfo to minimize duplication

--- ShareX.HistoryLib/Properties/AssemblyInfo.cs
+++ ShareX.HistoryLib/Properties/AssemblyInfo.cs
@@ -2,14 +2,4 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("ShareX HistoryLib")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("ShareX Team")]
-[assembly: AssemblyProduct("ShareX")]
-[assembly: AssemblyCopyright("Copyright (c) 2007-2018 ShareX Team")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: ComVisible(false)]
 [assembly: Guid("158992c9-b38c-4d85-b95b-1b46e7f75940")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SharedAssemblyInfo.cs
@@ -0,0 +1,13 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyDescription("Screen capture, file sharing and productivity tool")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("ShareX Team")]
+[assembly: AssemblyProduct("ShareX")]
+[assembly: AssemblyCopyright("Copyright (c) 2007-2018 ShareX Team")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("12.1.0")]
+[assembly: AssemblyFileVersion("12.1.0")]
