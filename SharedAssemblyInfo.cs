f16b91ae454c675ea74c46a725c4412381a2231d
Charles Milette <charles.milette@gmail.com>
2018-01-01 13:01:31 -0500

    Remove empty fields

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -1,12 +1,9 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("ShareX Team")]
 [assembly: AssemblyProduct("ShareX")]
 [assembly: AssemblyCopyright("Copyright (c) 2007-2018 ShareX Team")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: AssemblyVersion("12.1.0")]
 [assembly: AssemblyFileVersion("12.1.0")]
