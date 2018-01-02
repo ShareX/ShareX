178dba4905fc6bcc1f5f07d57e1945b8611aba22
Charles Milette <charles.milette@gmail.com>
2018-01-02 12:18:47 -0500

    Man I am good at PowerShell

--- AppVeyor/before_build.ps1
+++ AppVeyor/before_build.ps1
@@ -6,20 +6,20 @@ if ($env:APPVEYOR_PULL_REQUEST_NUMBER -eq $null)
 nuget restore ShareX.sln
 
 # Remove when https://github.com/NuGet/Home/issues/4778 is fixed
+$net40 =  @"
+      "originalTargetFrameworks": [
+        ".NETFramework,Version=v4.0"
+      ],
+"@
+
+$net46 = @"
+      "originalTargetFrameworks": [
+        ".NETFramework,Version=v4.6"
+      ],
+"@
+
 if ($env:CONFIGURATION -eq "WindowsStore")
 {
-    $net40 =  @"
-          "originalTargetFrameworks": [
-            ".NETFramework,Version=v4.0"
-          ],
-    "@
-
-    $net46 = @"
-          "originalTargetFrameworks": [
-            ".NETFramework,Version=v4.6"
-          ],
-    "@
-
     $fileContent = Get-Content "ShareX\obj\project.assets.json" -Raw
     $newFileContent = $fileContent -replace $net40, $net46
     Set-Content -Path "ShareX\obj\project.assets.json" -Value $newFileContent
