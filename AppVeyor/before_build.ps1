3199bc9d92b825cb7d81b6db2d47c51910645d45
Charles Milette <charles.milette@gmail.com>
2018-01-02 13:07:11 -0500

    Completely replace nuget restore by the msbuild restore task

--- AppVeyor/before_build.ps1
+++ AppVeyor/before_build.ps1
@@ -3,11 +3,4 @@ if ($env:APPVEYOR_PULL_REQUEST_NUMBER -eq $null)
     Invoke-WebRequest "$env:APIKeys" -OutFile "ShareX.UploadersLib\APIKeys\APIKeysLocal.cs"
 }
 
-nuget restore ShareX.sln
-
-if ($env:CONFIGURATION -eq "WindowsStore")
-{
-    # Main project uses a different Framework version under the Windows Store configuration,
-    # but NuGet restore doesn't knows, so have MsBuild do the job (which knows it)
-    msbuild /t:restore /p:Configuration=WindowsStore ShareX\ShareX.csproj
-}
+msbuild /t:restore /p:Configuration=$env:CONFIGURATION ShareX\ShareX.csproj
