6cbfb1a9ed589cfccd27c7ff9289fc231ecbacde
Charles Milette <charles.milette@gmail.com>
2018-01-02 13:22:15 -0500

    AAAAAAAAAAAAAAAAAA

--- AppVeyor/before_build.ps1
+++ AppVeyor/before_build.ps1
@@ -3,4 +3,4 @@ if ($env:APPVEYOR_PULL_REQUEST_NUMBER -eq $null)
     Invoke-WebRequest "$env:APIKeys" -OutFile "ShareX.UploadersLib\APIKeys\APIKeysLocal.cs"
 }
 
-msbuild /t:restore /p:Configuration=$env:CONFIGURATION ShareX\ShareX.csproj
+msbuild /t:restore /p:Configuration=$env:CONFIGURATION ShareX.sln
