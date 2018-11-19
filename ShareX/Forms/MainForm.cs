431ffe418e2af02082d31e03217b9cc1391d20f9
Charles Milette <me@charlesmilette.net>
2018-11-19 14:17:50 -0500

    Make code prettier

--- ShareX/Forms/MainForm.cs
+++ ShareX/Forms/MainForm.cs
@@ -302,7 +302,7 @@ namespace ShareX
         {
             if (m.Msg == (int)WindowsMessages.QUERYENDSESSION)
             {
-                EndSessionReasons reason = (EndSessionReasons)(m.LParam.ToInt64() & 0xFFFFFFFF);
+                EndSessionReasons reason = (EndSessionReasons)(long)(m.LParam);
                 if (reason.HasFlag(EndSessionReasons.ENDSESSION_CLOSEAPP))
                 {
                     // Register for restart. This allows our application to automatically restart when it is installing an update from the Store.
