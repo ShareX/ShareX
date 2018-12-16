6014438ea6500ff1247455ead5abbf6f05862c42
Charles Milette <me@charlesmilette.net>
2018-12-16 00:00:08 -0500

    Add license information, fix crash when opening webpage capture on 7 and when closing webpage capture on 10

--- ShareX.ScreenCaptureLib/WebpageCapture/WebpageCaptureBase.cs
+++ ShareX.ScreenCaptureLib/WebpageCapture/WebpageCaptureBase.cs
@@ -1,4 +1,29 @@
-﻿using ShareX.HelpersLib;
+﻿#region License Information (GPL v3)
+
+/*
+    ShareX - A program that allows you to take screenshots and share any file type
+    Copyright (c) 2007-2018 ShareX Team
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+    Optionally you can also view the license at <http://www.gnu.org/licenses/>.
+*/
+
+#endregion License Information (GPL v3)
+
+using ShareX.HelpersLib;
 using System;
 using System.Drawing;
 using System.IO;
@@ -14,7 +39,9 @@ namespace ShareX.ScreenCaptureLib.WebpageCapture
         public int CaptureDelay { get; set; }
 
         public abstract void CapturePage(string url, Size browserSize);
+
         public abstract void Stop();
+
         public abstract void Dispose();
 
         public void CapturePage(string url)
@@ -36,14 +63,13 @@ namespace ShareX.ScreenCaptureLib.WebpageCapture
 
         private static bool WebViewSupported()
         {
-            return Helpers.IsWindows10OrGreater() &&
-                ApiInformation.IsApiContractPresent("Windows.Foundation.UniversalApiContract", 6) && // 6th API contract is minimum for WebView support.
+            return ApiInformation.IsApiContractPresent("Windows.Foundation.UniversalApiContract", 6) && // 6th API contract is minimum for WebView support.
                 File.Exists(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.System), "edgehtml.dll")); // Make sure EdgeHTML is present.
         }
 
         public static WebpageCaptureBase Create()
         {
-            if (WebViewSupported())
+            if (Helpers.IsWindows10OrGreater() && WebViewSupported())
             {
                 try
                 {
