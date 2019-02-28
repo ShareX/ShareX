1b6fc73c9bfacf3bd36a4746a6496971b28ef1f5
Jaex <Jaex@users.noreply.github.com>
2019-02-28 17:58:11 +0300

    Update enabled states of RichTextBox context menu

--- ShareX.HelpersLib/Extensions/Extensions.cs
+++ ShareX.HelpersLib/Extensions/Extensions.cs
@@ -229,16 +229,30 @@ namespace ShareX.HelpersLib
         {
             if (rtb.ContextMenuStrip == null)
             {
-                ContextMenuStrip cms = new ContextMenuStrip { ShowImageMargin = false };
+                ContextMenuStrip cms = new ContextMenuStrip()
+                {
+                    ShowImageMargin = false
+                };
+
                 ToolStripMenuItem tsmiCut = new ToolStripMenuItem(Resources.Extensions_AddContextMenu_Cut);
                 tsmiCut.Click += (sender, e) => rtb.Cut();
                 cms.Items.Add(tsmiCut);
+
                 ToolStripMenuItem tsmiCopy = new ToolStripMenuItem(Resources.Extensions_AddContextMenu_Copy);
                 tsmiCopy.Click += (sender, e) => rtb.Copy();
                 cms.Items.Add(tsmiCopy);
+
                 ToolStripMenuItem tsmiPaste = new ToolStripMenuItem(Resources.Extensions_AddContextMenu_Paste);
                 tsmiPaste.Click += (sender, e) => rtb.Paste();
                 cms.Items.Add(tsmiPaste);
+
+                cms.Opening += (sender, e) =>
+                {
+                    tsmiCut.Enabled = !rtb.ReadOnly && rtb.SelectionLength > 0;
+                    tsmiCopy.Enabled = rtb.SelectionLength > 0;
+                    tsmiPaste.Enabled = !rtb.ReadOnly && Clipboard.ContainsText();
+                };
+
                 rtb.ContextMenuStrip = cms;
             }
         }
