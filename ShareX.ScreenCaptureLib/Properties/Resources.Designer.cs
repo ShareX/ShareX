5a660e390f1382697766ce3c1b21981c874b2f6c
Jaex <Jaex@users.noreply.github.com>
2019-04-08 00:26:57 +0300

    Change cursor drawing tool icon

--- ShareX.ScreenCaptureLib/Properties/Resources.Designer.cs
+++ ShareX.ScreenCaptureLib/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace ShareX.ScreenCaptureLib.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -1657,6 +1657,16 @@ namespace ShareX.ScreenCaptureLib.Properties {
         internal static System.Drawing.Bitmap smiley_yell {
             get {
                 object obj = ResourceManager.GetObject("smiley_yell", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap stamp_cursor {
+            get {
+                object obj = ResourceManager.GetObject("stamp_cursor", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
