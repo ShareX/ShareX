e42af50a937595e216bf771b1344a8b9bb89d7c8
lucario <10692069+lucario@users.noreply.github.com>
2020-02-18 21:38:44 -0500

    Basic implementation of Skynet API

---
+++ ShareX.UploadersLib/FileUploaders/Skynet.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+
+
+namespace ShareX.UploadersLib.FileUploaders
+{
+    public class SkynetNewFileUploaderService : FileUploaderService
+    {
+        public override FileDestination EnumValue { get; } = FileDestination.Skynet;
+
+        public override bool CheckConfig(UploadersConfig config)
+        {
+            return true;
+        }
+
+        public override GenericUploader CreateUploader(UploadersConfig config, TaskReferenceHelper taskInfo)
+        {
+            return new Skynet();
+        }
+    }
+    public sealed class Skynet : FileUploader
+    {
+        string SkynetDomain = "siasky.net";
+
+        public override UploadResult Upload(Stream stream, string fileName)
+        {
+            string SkynetEndpoint = $"https://{SkynetDomain}/skynet/skyfile/{Guid.NewGuid().ToString()}";
+
+            UploadResult result = SendRequestFile(SkynetEndpoint, stream, fileName, "file");
+
+            SkynetResponse response = JsonConvert.DeserializeObject<SkynetResponse>(result.Response);
+
+            result.URL = $"https://{SkynetDomain}/{response.skylink}";
+
+            return result;
+        }
+
+        private class SkynetResponse
+        {
+            public string skylink { get; set; }
+        }
+
+    }
+}
