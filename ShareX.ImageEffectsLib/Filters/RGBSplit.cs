5fdf602200a82093214b0ef561ba3cda958547d0
Jaex <Jaex@users.noreply.github.com>
2020-07-04 00:51:47 +0300

    Code refactor

--- ShareX.ImageEffectsLib/Filters/RGBSplit.cs
+++ ShareX.ImageEffectsLib/Filters/RGBSplit.cs
@@ -35,8 +35,10 @@ namespace ShareX.ImageEffectsLib
     {
         [DefaultValue(typeof(Point), "-5, 0")]
         public Point OffsetRed { get; set; } = new Point(-5, 0);
+
         [DefaultValue(typeof(Point), "0, 0")]
         public Point OffsetGreen { get; set; }
+
         [DefaultValue(typeof(Point), "5, 0")]
         public Point OffsetBlue { get; set; } = new Point(5, 0);
 
@@ -47,21 +49,18 @@ namespace ShareX.ImageEffectsLib
             using (UnsafeBitmap source = new UnsafeBitmap(bmp, true, ImageLockMode.ReadOnly))
             using (UnsafeBitmap dest = new UnsafeBitmap(bmpResult, true, ImageLockMode.WriteOnly))
             {
-                for (int y = 0; y < dest.Height; y++)
+                int right = source.Width - 1;
+                int bottom = source.Height - 1;
+
+                for (int y = 0; y < source.Height; y++)
                 {
-                    for (int x = 0; x < dest.Width; x++)
+                    for (int x = 0; x < source.Width; x++)
                     {
-                        ColorBgra colorR = source.GetPixel(MathHelpers.Clamp(x + OffsetRed.X, 0, dest.Width - 1), MathHelpers.Clamp(y + OffsetRed.Y, 0, dest.Height - 1));
-                        ColorBgra colorG = source.GetPixel(MathHelpers.Clamp(x + OffsetGreen.X, 0, dest.Width - 1), MathHelpers.Clamp(y + OffsetGreen.Y, 0, dest.Height - 1));
-                        ColorBgra colorB = source.GetPixel(MathHelpers.Clamp(x + OffsetBlue.X, 0, dest.Width - 1), MathHelpers.Clamp(y + OffsetBlue.Y, 0, dest.Height - 1));
-
-                        byte colorR_alpha = colorR.Alpha;
-                        byte colorG_alpha = colorG.Alpha;
-                        byte colorB_alpha = colorB.Alpha;
-                        byte colorA = (byte)(colorR_alpha / 3 + colorG_alpha / 3 + colorB_alpha / 3);
-
-                        ColorBgra shiftedColor = new ColorBgra((byte)(colorB.Blue * colorB_alpha / 255), (byte)(colorG.Green * colorG_alpha / 255),
-                            (byte)(colorR.Red * colorR_alpha / 255), colorA);
+                        ColorBgra colorR = source.GetPixel(MathHelpers.Clamp(x - OffsetRed.X, 0, right), MathHelpers.Clamp(y - OffsetRed.Y, 0, bottom));
+                        ColorBgra colorG = source.GetPixel(MathHelpers.Clamp(x - OffsetGreen.X, 0, right), MathHelpers.Clamp(y - OffsetGreen.Y, 0, bottom));
+                        ColorBgra colorB = source.GetPixel(MathHelpers.Clamp(x - OffsetBlue.X, 0, right), MathHelpers.Clamp(y - OffsetBlue.Y, 0, bottom));
+                        ColorBgra shiftedColor = new ColorBgra((byte)(colorB.Blue * colorB.Alpha / 255), (byte)(colorG.Green * colorG.Alpha / 255),
+                            (byte)(colorR.Red * colorR.Alpha / 255), (byte)((colorR.Alpha + colorG.Alpha + colorB.Alpha) / 3));
                         dest.SetPixel(x, y, shiftedColor);
                     }
                 }
