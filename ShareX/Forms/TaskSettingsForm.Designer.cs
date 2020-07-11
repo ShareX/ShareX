6bcb9e67ea38eb90c147d3937e4a8f1be73c68f7
Michael Delpach <McoreD@users.noreply.github.com>
2020-07-11 13:23:07 +0800

    TaskSettingsForm DPI issue fixed and tested

--- ShareX/Forms/TaskSettingsForm.Designer.cs
+++ ShareX/Forms/TaskSettingsForm.Designer.cs
@@ -2157,7 +2157,8 @@
             // TaskSettingsForm
             // 
             resources.ApplyResources(this, "$this");
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.BackColor = System.Drawing.SystemColors.Window;
             this.Controls.Add(this.tcTaskSettings);
             this.Controls.Add(this.tttvMain);
