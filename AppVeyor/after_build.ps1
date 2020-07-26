5c4ba951563d1e6509f5c53d7577d215d5206c14
Jaex <Jaex@users.noreply.github.com>
2020-07-26 14:12:17 +0300

    Revert "Removed APPVEYOR_REPO_TAG check"
    This reverts commit 163ca4092791ffee97ba883e389ccdca07c5b87f.

--- AppVeyor/after_build.ps1
+++ AppVeyor/after_build.ps1
@@ -4,12 +4,23 @@ if ($env:APPVEYOR_PULL_REQUEST_NUMBER -eq $null)
     {
         & "ShareX.Setup\bin\Release\ShareX.Setup.exe" -AppVeyorRelease
     }
+    elseif ($env:APPVEYOR_REPO_TAG -eq $true)
+    {
+        if ($env:CONFIGURATION -eq "Steam")
+        {
+            & "ShareX.Setup\bin\Steam\ShareX.Setup.exe" -AppVeyorSteamRelease
+        }
+        elseif ($env:CONFIGURATION -eq "WindowsStore")
+        {
+            & "ShareX.Setup\bin\WindowsStore\ShareX.Setup.exe" -AppVeyorWindowsStoreRelease
+        }
+    }
     elseif ($env:CONFIGURATION -eq "Steam")
     {
-        & "ShareX.Setup\bin\Steam\ShareX.Setup.exe" -AppVeyorSteamRelease
+        & "ShareX.Setup\bin\Steam\ShareX.Setup.exe" -AppVeyorSteam
     }
     elseif ($env:CONFIGURATION -eq "WindowsStore")
     {
-        & "ShareX.Setup\bin\WindowsStore\ShareX.Setup.exe" -AppVeyorWindowsStoreRelease
+        & "ShareX.Setup\bin\WindowsStore\ShareX.Setup.exe" -AppVeyorWindowsStore
     }
 }
