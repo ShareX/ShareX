12a4873b9faeabea50677c403a8793a17a3c0535
Karl Tatom <BallisticLingonberries@users.noreply.github.com>
2020-08-30 21:42:42 -0700

    remove unneeded comment

--- ShareX.UploadersLib/Forms/UploadersConfigFormHelper.cs
+++ ShareX.UploadersLib/Forms/UploadersConfigFormHelper.cs
@@ -800,7 +800,7 @@ namespace ShareX.UploadersLib
                 Config.PushbulletSettings.DeviceList.ForEach(pbDevice =>
                 {
                     if (!String.IsNullOrEmpty(pbDevice.Name))
-                        cboPushbulletDevices.Items.Add(pbDevice.Name);// ?? Resources.UploadersConfigForm_LoadSettings_Invalid_device_name);
+                        cboPushbulletDevices.Items.Add(pbDevice.Name);
                 });
 
                 cboPushbulletDevices.SelectedIndex = 0;
