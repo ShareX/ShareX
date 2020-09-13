ea69c42047ea7c1ec56dbfb08d5a7b7b38680ffc
Jaex <Jaex@users.noreply.github.com>
2020-09-13 21:13:45 +0300

    Made form localizable

--- ShareX.HelpersLib/Forms/ClipboardViewerForm.designer.cs
+++ ShareX.HelpersLib/Forms/ClipboardViewerForm.designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ClipboardViewerForm));
             this.txtSelectedClipboardContent = new System.Windows.Forms.TextBox();
             this.btnRefresh = new System.Windows.Forms.Button();
             this.btnClearClipboard = new System.Windows.Forms.Button();
@@ -38,80 +39,57 @@
             // 
             // txtSelectedClipboardContent
             // 
-            this.txtSelectedClipboardContent.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.txtSelectedClipboardContent.Location = new System.Drawing.Point(264, 8);
-            this.txtSelectedClipboardContent.Multiline = true;
+            resources.ApplyResources(this.txtSelectedClipboardContent, "txtSelectedClipboardContent");
             this.txtSelectedClipboardContent.Name = "txtSelectedClipboardContent";
             this.txtSelectedClipboardContent.ReadOnly = true;
-            this.txtSelectedClipboardContent.Size = new System.Drawing.Size(512, 544);
-            this.txtSelectedClipboardContent.TabIndex = 3;
             // 
             // btnRefresh
             // 
-            this.btnRefresh.Location = new System.Drawing.Point(8, 8);
+            resources.ApplyResources(this.btnRefresh, "btnRefresh");
             this.btnRefresh.Name = "btnRefresh";
-            this.btnRefresh.Size = new System.Drawing.Size(120, 32);
-            this.btnRefresh.TabIndex = 0;
-            this.btnRefresh.Text = "Refresh";
             this.btnRefresh.UseVisualStyleBackColor = true;
             this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
             // 
             // btnClearClipboard
             // 
-            this.btnClearClipboard.Location = new System.Drawing.Point(136, 8);
+            resources.ApplyResources(this.btnClearClipboard, "btnClearClipboard");
             this.btnClearClipboard.Name = "btnClearClipboard";
-            this.btnClearClipboard.Size = new System.Drawing.Size(120, 32);
-            this.btnClearClipboard.TabIndex = 1;
-            this.btnClearClipboard.Text = "Clear clipboard";
             this.btnClearClipboard.UseVisualStyleBackColor = true;
             this.btnClearClipboard.Click += new System.EventHandler(this.btnClearClipboard_Click);
             // 
             // lvClipboardContentList
             // 
-            this.lvClipboardContentList.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left)));
+            resources.ApplyResources(this.lvClipboardContentList, "lvClipboardContentList");
             this.lvClipboardContentList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.chFormat});
             this.lvClipboardContentList.FullRowSelect = true;
             this.lvClipboardContentList.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
             this.lvClipboardContentList.HideSelection = false;
-            this.lvClipboardContentList.Location = new System.Drawing.Point(8, 48);
             this.lvClipboardContentList.MultiSelect = false;
             this.lvClipboardContentList.Name = "lvClipboardContentList";
-            this.lvClipboardContentList.Size = new System.Drawing.Size(248, 504);
-            this.lvClipboardContentList.TabIndex = 2;
             this.lvClipboardContentList.UseCompatibleStateImageBehavior = false;
             this.lvClipboardContentList.View = System.Windows.Forms.View.Details;
             this.lvClipboardContentList.SelectedIndexChanged += new System.EventHandler(this.lvClipboardContentList_SelectedIndexChanged);
             // 
             // chFormat
             // 
-            this.chFormat.Text = "Format";
-            this.chFormat.Width = 244;
+            resources.ApplyResources(this.chFormat, "chFormat");
             // 
             // pbSelectedClipboardContent
             // 
-            this.pbSelectedClipboardContent.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
+            resources.ApplyResources(this.pbSelectedClipboardContent, "pbSelectedClipboardContent");
             this.pbSelectedClipboardContent.BackColor = System.Drawing.SystemColors.Window;
             this.pbSelectedClipboardContent.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.pbSelectedClipboardContent.DrawCheckeredBackground = true;
             this.pbSelectedClipboardContent.FullscreenOnClick = true;
-            this.pbSelectedClipboardContent.Location = new System.Drawing.Point(264, 8);
             this.pbSelectedClipboardContent.Name = "pbSelectedClipboardContent";
             this.pbSelectedClipboardContent.PictureBoxBackColor = System.Drawing.SystemColors.Window;
             this.pbSelectedClipboardContent.ShowImageSizeLabel = true;
-            this.pbSelectedClipboardContent.Size = new System.Drawing.Size(512, 544);
-            this.pbSelectedClipboardContent.TabIndex = 4;
             // 
             // ClipboardViewerForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(784, 561);
             this.Controls.Add(this.pbSelectedClipboardContent);
             this.Controls.Add(this.lvClipboardContentList);
             this.Controls.Add(this.btnClearClipboard);
@@ -119,8 +97,6 @@
             this.Controls.Add(this.txtSelectedClipboardContent);
             this.DoubleBuffered = true;
             this.Name = "ClipboardViewerForm";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "ShareX - Clipboard viewer";
             this.ResumeLayout(false);
             this.PerformLayout();
 
