d4bfdbd7b8e4b4735da4afec5164367bd9f37c42
Jaex <Jaex@users.noreply.github.com>
2021-05-31 09:38:41 +0300

    Added KnownTypesSerializationBinder

--- ShareX.HelpersLib/Settings/KnownTypesSerializationBinder.cs
+++ ShareX.HelpersLib/Settings/KnownTypesSerializationBinder.cs
@@ -23,15 +23,26 @@
 
 #endregion License Information (GPL v3)
 
-using ShareX.HelpersLib;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace ShareX.ImageEffectsLib
+namespace ShareX.HelpersLib
 {
-    public class ImageEffectsSerializationBinder : KnownTypesSerializationBinder
+    public class KnownTypesSerializationBinder : ISerializationBinder
     {
-        public ImageEffectsSerializationBinder()
+        public IEnumerable<Type> KnownTypes { get; set; }
+
+        public void BindToName(Type serializedType, out string assemblyName, out string typeName)
         {
-            KnownTypes = Helpers.FindSubclassesOf<ImageEffect>();
+            assemblyName = null;
+            typeName = serializedType.Name;
+        }
+
+        public Type BindToType(string assemblyName, string typeName)
+        {
+            return KnownTypes.SingleOrDefault(t => t.Name == typeName);
         }
     }
 }
