2ab1a0f0b6a6f9fda1bf407412bd10c780afd9fe
Jaex <Jaex@users.noreply.github.com>
2021-09-05 01:28:06 +0300

    Set dev true

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("ShareX")]
 [assembly: AssemblyCopyright("Copyright (c) 2007-2021 ShareX Team")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("13.6.0")]
-[assembly: AssemblyFileVersion("13.6.0")]
+[assembly: AssemblyVersion("13.6.1")]
+[assembly: AssemblyFileVersion("13.6.1")]
