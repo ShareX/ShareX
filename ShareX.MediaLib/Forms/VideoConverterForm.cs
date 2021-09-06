f383e3893cb9036bef428f9bf9b33b0d54632833
Jaex <Jaex@users.noreply.github.com>
2021-09-06 04:42:07 +0300

    Support drag n drop file to video converter form

--- ShareX.MediaLib/Forms/VideoConverterForm.cs
+++ ShareX.MediaLib/Forms/VideoConverterForm.cs
@@ -146,6 +146,21 @@ namespace ShareX.MediaLib
                 !string.IsNullOrEmpty(Options.OutputFileName);
         }
 
+        private void UpdateInputFilePathTextBox(string filePath)
+        {
+            txtInputFilePath.Text = filePath;
+
+            if (string.IsNullOrEmpty(txtOutputFolder.Text))
+            {
+                txtOutputFolder.Text = Path.GetDirectoryName(filePath);
+            }
+
+            if (string.IsNullOrEmpty(txtOutputFileName.Text))
+            {
+                txtOutputFileName.Text = Path.GetFileNameWithoutExtension(filePath) + "-output";
+            }
+        }
+
         private bool StartEncoding()
         {
             bool result = false;
@@ -198,17 +213,7 @@ namespace ShareX.MediaLib
                 if (ofd.ShowDialog(this) == DialogResult.OK)
                 {
                     string filePath = ofd.FileName;
-                    txtInputFilePath.Text = filePath;
-
-                    if (string.IsNullOrEmpty(txtOutputFolder.Text))
-                    {
-                        txtOutputFolder.Text = Path.GetDirectoryName(filePath);
-                    }
-
-                    if (string.IsNullOrEmpty(txtOutputFileName.Text))
-                    {
-                        txtOutputFileName.Text = Path.GetFileNameWithoutExtension(filePath) + "-output";
-                    }
+                    UpdateInputFilePathTextBox(filePath);
                 }
             }
         }
@@ -285,6 +290,27 @@ namespace ShareX.MediaLib
             else if (ffmpeg != null)
             {
                 ffmpeg.Close();
+            }
+        }
+
+        private void VideoConverterForm_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop, false))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        private void VideoConverterForm_DragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop, false) && e.Data.GetData(DataFormats.FileDrop, false) is string[] files && files.Length > 0)
+            {
+                string filePath = files[0];
+                UpdateInputFilePathTextBox(filePath);
             }
         }
 
