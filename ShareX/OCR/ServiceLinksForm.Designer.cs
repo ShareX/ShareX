caf7fca329137ac34ccfb31108f6476973aab639
Jaex <Jaex@users.noreply.github.com>
2022-05-04 12:36:00 +0300

    Set localizable true

--- ShareX/OCR/ServiceLinksForm.Designer.cs
+++ ShareX/OCR/ServiceLinksForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ServiceLinksForm));
             this.cbServices = new System.Windows.Forms.ComboBox();
             this.lblServices = new System.Windows.Forms.Label();
             this.btnNew = new System.Windows.Forms.Button();
@@ -43,90 +44,62 @@
             // 
             this.cbServices.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cbServices.FormattingEnabled = true;
-            this.cbServices.Location = new System.Drawing.Point(16, 64);
+            resources.ApplyResources(this.cbServices, "cbServices");
             this.cbServices.Name = "cbServices";
-            this.cbServices.Size = new System.Drawing.Size(376, 21);
-            this.cbServices.TabIndex = 4;
             this.cbServices.SelectedIndexChanged += new System.EventHandler(this.cbServices_SelectedIndexChanged);
             // 
             // lblServices
             // 
-            this.lblServices.AutoSize = true;
-            this.lblServices.Location = new System.Drawing.Point(13, 48);
+            resources.ApplyResources(this.lblServices, "lblServices");
             this.lblServices.Name = "lblServices";
-            this.lblServices.Size = new System.Drawing.Size(51, 13);
-            this.lblServices.TabIndex = 3;
-            this.lblServices.Text = "Services:";
             // 
             // btnNew
             // 
-            this.btnNew.Location = new System.Drawing.Point(16, 16);
+            resources.ApplyResources(this.btnNew, "btnNew");
             this.btnNew.Name = "btnNew";
-            this.btnNew.Size = new System.Drawing.Size(120, 24);
-            this.btnNew.TabIndex = 0;
-            this.btnNew.Text = "New";
             this.btnNew.UseVisualStyleBackColor = true;
             this.btnNew.Click += new System.EventHandler(this.btnNew_Click);
             // 
             // btnRemove
             // 
-            this.btnRemove.Location = new System.Drawing.Point(144, 16);
+            resources.ApplyResources(this.btnRemove, "btnRemove");
             this.btnRemove.Name = "btnRemove";
-            this.btnRemove.Size = new System.Drawing.Size(120, 24);
-            this.btnRemove.TabIndex = 1;
-            this.btnRemove.Text = "Remove";
             this.btnRemove.UseVisualStyleBackColor = true;
             this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
             // 
             // lblName
             // 
-            this.lblName.AutoSize = true;
-            this.lblName.Location = new System.Drawing.Point(13, 96);
+            resources.ApplyResources(this.lblName, "lblName");
             this.lblName.Name = "lblName";
-            this.lblName.Size = new System.Drawing.Size(38, 13);
-            this.lblName.TabIndex = 5;
-            this.lblName.Text = "Name:";
             // 
             // txtName
             // 
-            this.txtName.Location = new System.Drawing.Point(16, 112);
+            resources.ApplyResources(this.txtName, "txtName");
             this.txtName.Name = "txtName";
-            this.txtName.Size = new System.Drawing.Size(376, 20);
-            this.txtName.TabIndex = 6;
             this.txtName.TextChanged += new System.EventHandler(this.txtName_TextChanged);
             // 
             // lblURL
             // 
-            this.lblURL.AutoSize = true;
-            this.lblURL.Location = new System.Drawing.Point(13, 144);
+            resources.ApplyResources(this.lblURL, "lblURL");
             this.lblURL.Name = "lblURL";
-            this.lblURL.Size = new System.Drawing.Size(32, 13);
-            this.lblURL.TabIndex = 7;
-            this.lblURL.Text = "URL:";
             // 
             // txtURL
             // 
-            this.txtURL.Location = new System.Drawing.Point(16, 160);
+            resources.ApplyResources(this.txtURL, "txtURL");
             this.txtURL.Name = "txtURL";
-            this.txtURL.Size = new System.Drawing.Size(376, 20);
-            this.txtURL.TabIndex = 8;
             this.txtURL.TextChanged += new System.EventHandler(this.txtURL_TextChanged);
             // 
             // btnReset
             // 
-            this.btnReset.Location = new System.Drawing.Point(272, 16);
+            resources.ApplyResources(this.btnReset, "btnReset");
             this.btnReset.Name = "btnReset";
-            this.btnReset.Size = new System.Drawing.Size(120, 24);
-            this.btnReset.TabIndex = 2;
-            this.btnReset.Text = "Reset";
             this.btnReset.UseVisualStyleBackColor = true;
             this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
             // 
             // ServiceLinksForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(408, 193);
             this.Controls.Add(this.btnReset);
             this.Controls.Add(this.txtURL);
             this.Controls.Add(this.lblURL);
@@ -141,8 +114,6 @@
             this.MinimizeBox = false;
             this.Name = "ServiceLinksForm";
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "ShareX - Service links";
             this.ResumeLayout(false);
             this.PerformLayout();
 
