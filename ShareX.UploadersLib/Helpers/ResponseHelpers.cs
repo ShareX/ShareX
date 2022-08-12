541b7cb9d1999f697f390ff5aa074b9898ebf2b0
Chris Brown <chrisbrown1@hotmail.ca>
2022-08-12 16:23:04 -0400

    Implemented helper class to display URL redirect links after being decoded from QR code. Updated QRCodeForm to also display the redirected URL.

---
+++ ShareX.UploadersLib/Helpers/ResponseHelpers.cs
@@ -0,0 +1,29 @@
+﻿using ShareX.HelpersLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShareX.UploadersLib
+{
+    public class ResponseHelpers : Uploader
+    {
+        public static string UrlRedirectLink(string url)
+        {
+            if (!URLHelpers.IsValidURL(url)) return null;
+
+            ResponseHelpers responseHelper = new ResponseHelpers();
+            var response = responseHelper.GetResponse(HttpMethod.GET, url);
+
+            if (response == null) return null;
+
+            if (response.ResponseUri.ToString().ToLower() == url.ToLower()) return null;
+
+            return response.ResponseUri.ToString();
+        }
+
+    }
+}
--- ShareX/Forms/QRCodeForm.cs
+++ ShareX/Forms/QRCodeForm.cs
@@ -137,6 +137,11 @@ namespace ShareX
             if (results != null)
             {
                 output = string.Join(Environment.NewLine + Environment.NewLine, results);
+
+                if (UploadersLib.ResponseHelpers.UrlRedirectLink(output) != null)
+                {
+                    output = output + Environment.NewLine + Environment.NewLine + "This link redirects to: " + UploadersLib.ResponseHelpers.UrlRedirectLink(output);
+                }
             }
 
             rtbDecodeResult.Text = output;
