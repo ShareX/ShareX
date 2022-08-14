59c27f289634b3f17bb2f8b2b31a971c8d151c2e
Jaex <Jaex@users.noreply.github.com>
2022-08-14 16:13:34 +0300

    Increase version

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("ShareX")]
 [assembly: AssemblyCopyright("Copyright (c) 2007-2022 ShareX Team")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("14.1.1")]
-[assembly: AssemblyFileVersion("14.1.1")]
+[assembly: AssemblyVersion("14.1.2")]
+[assembly: AssemblyFileVersion("14.1.2")]
