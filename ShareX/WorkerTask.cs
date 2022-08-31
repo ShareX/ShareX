cabaaac3b8434637a35c849502f6b3755bac433e
Jaex <Jaex@users.noreply.github.com>
2022-08-31 13:02:56 +0300

    Revert "If system admin disabled upload then on first run disable after capture upload option"
    This reverts commit 0887008f4f0c3a67d1a62941fe93a319b5395714.

--- ShareX/WorkerTask.cs
+++ ShareX/WorkerTask.cs
@@ -377,7 +377,15 @@ namespace ShareX
 
                 if (disableUpload)
                 {
-                    SettingManager.DisableAfterCaptureUpload();
+                    Program.DefaultTaskSettings.AfterCaptureJob = Program.DefaultTaskSettings.AfterCaptureJob.Remove(AfterCaptureTasks.UploadImageToHost);
+
+                    foreach (HotkeySettings hotkeySettings in Program.HotkeysConfig.Hotkeys)
+                    {
+                        if (hotkeySettings.TaskSettings != null)
+                        {
+                            hotkeySettings.TaskSettings.AfterCaptureJob = hotkeySettings.TaskSettings.AfterCaptureJob.Remove(AfterCaptureTasks.UploadImageToHost);
+                        }
+                    }
 
                     Info.TaskSettings.AfterCaptureJob = Info.TaskSettings.AfterCaptureJob.Remove(AfterCaptureTasks.UploadImageToHost);
                     Info.Result.IsURLExpected = false;
