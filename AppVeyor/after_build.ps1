403b88fe2098bd727f93b034b3d7d4a5e8575f6f
Jaex <Jaex@users.noreply.github.com>
2022-10-15 21:05:31 +0300

    Code refactoring

--- AppVeyor/after_build.ps1
+++ AppVeyor/after_build.ps1
@@ -1,15 +1,4 @@
 if ($env:APPVEYOR_PULL_REQUEST_NUMBER -eq $null)
 {
-    if ($env:CONFIGURATION -eq "Release")
-    {
-        & "ShareX.Setup\bin\Release\ShareX.Setup.exe" -AppVeyor "$env:CONFIGURATION"
-    }
-    elseif ($env:CONFIGURATION -eq "Steam")
-    {
-        & "ShareX.Setup\bin\Steam\ShareX.Setup.exe" -AppVeyor "$env:CONFIGURATION"
-    }
-    elseif ($env:CONFIGURATION -eq "MicrosoftStore")
-    {
-        & "ShareX.Setup\bin\MicrosoftStore\ShareX.Setup.exe" -AppVeyor "$env:CONFIGURATION"
-    }
+    & "ShareX.Setup\bin\$env:CONFIGURATION\ShareX.Setup.exe" -AppVeyor "$env:CONFIGURATION"
 }
