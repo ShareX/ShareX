6365ecc60c2fef61d9f1f9d262aceb098fe712ea
Jaex <Jaex@users.noreply.github.com>
2022-10-15 22:22:57 +0300

    Remove AppVeyor folder

--- AppVeyor/after_build.ps1
+++
@@ -1,4 +0,0 @@
-if ($env:APPVEYOR_PULL_REQUEST_NUMBER -eq $null)
-{
-    & "ShareX.Setup\bin\$env:CONFIGURATION\ShareX.Setup.exe" -AppVeyor "$env:CONFIGURATION"
-}
--- AppVeyor/before_build.ps1
+++
@@ -1,6 +0,0 @@
-if ($env:APPVEYOR_PULL_REQUEST_NUMBER -eq $null)
-{
-    Invoke-WebRequest "$env:APIKeys" -OutFile "ShareX.UploadersLib\APIKeys\APIKeysLocal.cs"
-}
-
-nuget restore ShareX.sln
