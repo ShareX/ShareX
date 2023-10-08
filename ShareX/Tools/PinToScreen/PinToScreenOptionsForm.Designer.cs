4cfe3eb2d3fa8be5dc8880cbcd78782641404df7
Jaex <Jaex@users.noreply.github.com>
2023-10-08 06:53:06 +0300

    Set Localizable true

--- ShareX/Tools/PinToScreen/PinToScreenOptionsForm.Designer.cs
+++ ShareX/Tools/PinToScreen/PinToScreenOptionsForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PinToScreenOptionsForm));
             this.btnOK = new System.Windows.Forms.Button();
             this.btnCancel = new System.Windows.Forms.Button();
             this.lblPlacement = new System.Windows.Forms.Label();
@@ -53,116 +54,77 @@
             // 
             // btnOK
             // 
-            this.btnOK.Location = new System.Drawing.Point(192, 336);
+            resources.ApplyResources(this.btnOK, "btnOK");
             this.btnOK.Name = "btnOK";
-            this.btnOK.Size = new System.Drawing.Size(104, 32);
-            this.btnOK.TabIndex = 15;
-            this.btnOK.Text = "OK";
             this.btnOK.UseVisualStyleBackColor = true;
             this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
             // 
             // btnCancel
             // 
-            this.btnCancel.Location = new System.Drawing.Point(304, 336);
+            resources.ApplyResources(this.btnCancel, "btnCancel");
             this.btnCancel.Name = "btnCancel";
-            this.btnCancel.Size = new System.Drawing.Size(104, 32);
-            this.btnCancel.TabIndex = 16;
-            this.btnCancel.Text = "Cancel";
             this.btnCancel.UseVisualStyleBackColor = true;
             this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
             // 
             // lblPlacement
             // 
-            this.lblPlacement.AutoSize = true;
-            this.lblPlacement.Location = new System.Drawing.Point(13, 16);
+            resources.ApplyResources(this.lblPlacement, "lblPlacement");
             this.lblPlacement.Name = "lblPlacement";
-            this.lblPlacement.Size = new System.Drawing.Size(74, 16);
-            this.lblPlacement.TabIndex = 0;
-            this.lblPlacement.Text = "Placement:";
             // 
             // cbPlacement
             // 
             this.cbPlacement.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cbPlacement.FormattingEnabled = true;
-            this.cbPlacement.Location = new System.Drawing.Point(16, 40);
+            resources.ApplyResources(this.cbPlacement, "cbPlacement");
             this.cbPlacement.Name = "cbPlacement";
-            this.cbPlacement.Size = new System.Drawing.Size(144, 24);
-            this.cbPlacement.TabIndex = 1;
             // 
             // lblPlacementOffset
             // 
-            this.lblPlacementOffset.AutoSize = true;
-            this.lblPlacementOffset.Location = new System.Drawing.Point(173, 16);
+            resources.ApplyResources(this.lblPlacementOffset, "lblPlacementOffset");
             this.lblPlacementOffset.Name = "lblPlacementOffset";
-            this.lblPlacementOffset.Size = new System.Drawing.Size(109, 16);
-            this.lblPlacementOffset.TabIndex = 2;
-            this.lblPlacementOffset.Text = "Placement offset:";
             // 
             // nudPlacementOffset
             // 
-            this.nudPlacementOffset.Location = new System.Drawing.Point(176, 41);
+            resources.ApplyResources(this.nudPlacementOffset, "nudPlacementOffset");
             this.nudPlacementOffset.Maximum = new decimal(new int[] {
             300,
             0,
             0,
             0});
             this.nudPlacementOffset.Name = "nudPlacementOffset";
-            this.nudPlacementOffset.Size = new System.Drawing.Size(72, 22);
-            this.nudPlacementOffset.TabIndex = 3;
-            this.nudPlacementOffset.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // cbTopMost
             // 
-            this.cbTopMost.AutoSize = true;
-            this.cbTopMost.Location = new System.Drawing.Point(16, 80);
+            resources.ApplyResources(this.cbTopMost, "cbTopMost");
             this.cbTopMost.Name = "cbTopMost";
-            this.cbTopMost.Size = new System.Drawing.Size(83, 20);
-            this.cbTopMost.TabIndex = 4;
-            this.cbTopMost.Text = "Top most";
             this.cbTopMost.UseVisualStyleBackColor = true;
             // 
             // cbKeepCenterLocation
             // 
-            this.cbKeepCenterLocation.AutoSize = true;
-            this.cbKeepCenterLocation.Location = new System.Drawing.Point(16, 112);
+            resources.ApplyResources(this.cbKeepCenterLocation, "cbKeepCenterLocation");
             this.cbKeepCenterLocation.Name = "cbKeepCenterLocation";
-            this.cbKeepCenterLocation.Size = new System.Drawing.Size(148, 20);
-            this.cbKeepCenterLocation.TabIndex = 5;
-            this.cbKeepCenterLocation.Text = "Keep center location";
             this.cbKeepCenterLocation.UseVisualStyleBackColor = true;
             // 
             // cbShadow
             // 
-            this.cbShadow.AutoSize = true;
-            this.cbShadow.Location = new System.Drawing.Point(16, 144);
+            resources.ApplyResources(this.cbShadow, "cbShadow");
             this.cbShadow.Name = "cbShadow";
-            this.cbShadow.Size = new System.Drawing.Size(75, 20);
-            this.cbShadow.TabIndex = 6;
-            this.cbShadow.Text = "Shadow";
             this.cbShadow.UseVisualStyleBackColor = true;
             // 
             // cbBorder
             // 
-            this.cbBorder.AutoSize = true;
-            this.cbBorder.Location = new System.Drawing.Point(16, 176);
+            resources.ApplyResources(this.cbBorder, "cbBorder");
             this.cbBorder.Name = "cbBorder";
-            this.cbBorder.Size = new System.Drawing.Size(67, 20);
-            this.cbBorder.TabIndex = 7;
-            this.cbBorder.Text = "Border";
             this.cbBorder.UseVisualStyleBackColor = true;
             // 
             // lblBorderSize
             // 
-            this.lblBorderSize.AutoSize = true;
-            this.lblBorderSize.Location = new System.Drawing.Point(13, 208);
+            resources.ApplyResources(this.lblBorderSize, "lblBorderSize");
             this.lblBorderSize.Name = "lblBorderSize";
-            this.lblBorderSize.Size = new System.Drawing.Size(78, 16);
-            this.lblBorderSize.TabIndex = 8;
-            this.lblBorderSize.Text = "Border size:";
             // 
             // nudBorderSize
             // 
-            this.nudBorderSize.Location = new System.Drawing.Point(16, 232);
+            resources.ApplyResources(this.nudBorderSize, "nudBorderSize");
             this.nudBorderSize.Maximum = new decimal(new int[] {
             30,
             0,
@@ -174,9 +136,6 @@
             0,
             0});
             this.nudBorderSize.Name = "nudBorderSize";
-            this.nudBorderSize.Size = new System.Drawing.Size(72, 22);
-            this.nudBorderSize.TabIndex = 9;
-            this.nudBorderSize.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.nudBorderSize.Value = new decimal(new int[] {
             1,
             0,
@@ -187,63 +146,44 @@
             // 
             this.btnBorderColor.Color = System.Drawing.Color.Empty;
             this.btnBorderColor.ColorPickerOptions = null;
-            this.btnBorderColor.Location = new System.Drawing.Point(104, 230);
+            resources.ApplyResources(this.btnBorderColor, "btnBorderColor");
             this.btnBorderColor.Name = "btnBorderColor";
-            this.btnBorderColor.Size = new System.Drawing.Size(160, 26);
-            this.btnBorderColor.TabIndex = 10;
-            this.btnBorderColor.Text = "Border color";
-            this.btnBorderColor.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.btnBorderColor.UseVisualStyleBackColor = true;
             // 
             // lblMinimizeSize
             // 
-            this.lblMinimizeSize.AutoSize = true;
-            this.lblMinimizeSize.Location = new System.Drawing.Point(13, 272);
+            resources.ApplyResources(this.lblMinimizeSize, "lblMinimizeSize");
             this.lblMinimizeSize.Name = "lblMinimizeSize";
-            this.lblMinimizeSize.Size = new System.Drawing.Size(89, 16);
-            this.lblMinimizeSize.TabIndex = 11;
-            this.lblMinimizeSize.Text = "Minimize size:";
             // 
             // nudMinimizeSizeWidth
             // 
-            this.nudMinimizeSizeWidth.Location = new System.Drawing.Point(16, 296);
+            resources.ApplyResources(this.nudMinimizeSizeWidth, "nudMinimizeSizeWidth");
             this.nudMinimizeSizeWidth.Maximum = new decimal(new int[] {
             1000,
             0,
             0,
             0});
             this.nudMinimizeSizeWidth.Name = "nudMinimizeSizeWidth";
-            this.nudMinimizeSizeWidth.Size = new System.Drawing.Size(72, 22);
-            this.nudMinimizeSizeWidth.TabIndex = 12;
-            this.nudMinimizeSizeWidth.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // nudMinimizeSizeHeight
             // 
-            this.nudMinimizeSizeHeight.Location = new System.Drawing.Point(120, 296);
+            resources.ApplyResources(this.nudMinimizeSizeHeight, "nudMinimizeSizeHeight");
             this.nudMinimizeSizeHeight.Maximum = new decimal(new int[] {
             1000,
             0,
             0,
             0});
             this.nudMinimizeSizeHeight.Name = "nudMinimizeSizeHeight";
-            this.nudMinimizeSizeHeight.Size = new System.Drawing.Size(72, 22);
-            this.nudMinimizeSizeHeight.TabIndex = 14;
-            this.nudMinimizeSizeHeight.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // lblMinimizeSizeX
             // 
-            this.lblMinimizeSizeX.AutoSize = true;
-            this.lblMinimizeSizeX.Location = new System.Drawing.Point(98, 299);
+            resources.ApplyResources(this.lblMinimizeSizeX, "lblMinimizeSizeX");
             this.lblMinimizeSizeX.Name = "lblMinimizeSizeX";
-            this.lblMinimizeSizeX.Size = new System.Drawing.Size(13, 16);
-            this.lblMinimizeSizeX.TabIndex = 13;
-            this.lblMinimizeSizeX.Text = "x";
             // 
             // PinToScreenOptionsForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(420, 380);
             this.Controls.Add(this.lblMinimizeSizeX);
             this.Controls.Add(this.nudMinimizeSizeHeight);
             this.Controls.Add(this.nudMinimizeSizeWidth);
@@ -261,13 +201,9 @@
             this.Controls.Add(this.lblPlacement);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnOK);
-            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
             this.Name = "PinToScreenOptionsForm";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "ShareX - Pin to screen options";
             ((System.ComponentModel.ISupportInitialize)(this.nudPlacementOffset)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.nudBorderSize)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.nudMinimizeSizeWidth)).EndInit();
