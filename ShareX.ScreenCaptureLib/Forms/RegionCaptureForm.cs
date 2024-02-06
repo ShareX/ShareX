8b94ddbd1a395f046b4ddfb10e2da62664766878
Jaex <Jaex@users.noreply.github.com>
2024-02-06 11:37:58 +0300

    fixed #3495: In image editor use "Ctrl + C" instead of "Ctrl + Shift + C" to copy image to clipboard

--- ShareX.ScreenCaptureLib/Forms/RegionCaptureForm.cs
+++ ShareX.ScreenCaptureLib/Forms/RegionCaptureForm.cs
@@ -661,9 +661,6 @@ namespace ShareX.ScreenCaptureLib
                 case Keys.Oemtilde:
                     CloseWindow(RegionResult.ActiveMonitor);
                     break;
-                case Keys.Control | Keys.C:
-                    CopyAreaInfo();
-                    break;
             }
 
             if (IsEditorMode)
@@ -691,7 +688,11 @@ namespace ShareX.ScreenCaptureLib
             }
             else
             {
-                if (e.KeyData >= Keys.D0 && e.KeyData <= Keys.D9)
+                if (e.KeyData == (Keys.Control | Keys.C))
+                {
+                    CopyAreaInfo();
+                }
+                else if (e.KeyData >= Keys.D0 && e.KeyData <= Keys.D9)
                 {
                     MonitorKey(e.KeyData - Keys.D0);
                     return;
