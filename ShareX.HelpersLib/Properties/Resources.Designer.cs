4b03ad6ca4313cde5c7b1c27e55ca212a53b124c
Karl Tatom <BallisticLingonberries@users.noreply.github.com>
2025-09-21 05:51:15 -0700

    Implemented 3rd comment on issue #7983. Stil needs icon

--- ShareX.HelpersLib/Properties/Resources.Designer.cs
+++ ShareX.HelpersLib/Properties/Resources.Designer.cs
@@ -1809,7 +1809,18 @@ namespace ShareX.HelpersLib.Properties {
                 return ResourceManager.GetString("HotkeyType_StartAutoCapture", resourceCulture);
             }
         }
-        
+
+        /// <summary>
+        ///   Looks up a localized string similar to Stop auto capture.
+        /// </summary>
+        internal static string HotkeyType_StopAutoCapture
+        {
+            get
+            {
+                return ResourceManager.GetString("HotkeyType_StopAutoCapture", resourceCulture);
+            }
+        }
+
         /// <summary>
         ///   Looks up a localized string similar to Start/Stop screen recording using last region.
         /// </summary>
@@ -1827,7 +1838,7 @@ namespace ShareX.HelpersLib.Properties {
                 return ResourceManager.GetString("HotkeyType_StartScreenRecorderGIF", resourceCulture);
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to Stop screen recording.
         /// </summary>
