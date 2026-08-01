e85d658c6d17694ffa453a6c4ee7a96cefc36c39
Jaex <Jaex@users.noreply.github.com>
2026-08-01 14:44:39 +0300

    Remove obsolete files

--- ShareX/Localization/Strings.Designer.cs
+++ ShareX/Localization/Strings.Designer.cs
@@ -39,7 +39,7 @@ namespace ShareX.Localization {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ShareX.Resources.Strings", typeof(Strings).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ShareX.Localization.Strings", typeof(Strings).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
